7a1207f3821a79f5b721fa29e4562d035986a615
Ahmed Osama <142287501+Megawy@users.noreply.github.com>
2024-03-03 00:26:51 +0200

    fixes
    Fix Mail And add animation for header

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,67 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var contactForm = document.getElementById("contactForm");
-  contactForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+(function () {
+  emailjs.init("vyyEptGFaHUEU3Pds"); // Replace with your User ID
 
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var subject = document.getElementById("subject").value;
-    var message = document.getElementById("message").value;
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
 
-    var sendMessageButton = document.getElementById("sendMessageButton");
-    sendMessageButton.disabled = true;
+      // Collect user data from the form
+      var formData = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+      };
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "../../assets/php/send_email.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          var successDiv = document.getElementById("success");
-          successDiv.innerHTML =
-            "<div class='alert alert-success'>" +
-            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>" +
-            "<strong>Your message has been sent.</strong></div>";
-          // Reset form fields individually
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("subject").value = "";
-          document.getElementById("message").value = "";
-        } else {
-          var errorDiv = document.getElementById("success");
-          var errorMessage =
-            "<div class='alert alert-danger'>" +
-            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>" +
-            "<strong>Sorry, there was an error processing your request. Please try again later!</strong></div>";
-          errorDiv.innerHTML = errorMessage;
-        }
-        sendMessageButton.disabled = false;
+      // Check if any field is empty
+      if (
+        formData.name === "" ||
+        formData.email === "" ||
+        formData.subject === "" ||
+        formData.message === ""
+      ) {
+        document.getElementById("success").textContent =
+          "Please fill in all fields before sending the message.";
+        return;
       }
-    };
-    var formData =
-      "name=" +
-      encodeURIComponent(name) +
-      "&email=" +
-      encodeURIComponent(email) +
-      "&subject=" +
-      encodeURIComponent(subject) +
-      "&message=" +
-      encodeURIComponent(message);
-    xhr.send(formData);
-  });
 
-  var nameInput = document.getElementById("name");
-  nameInput.addEventListener("focus", function () {
-    var successDiv = document.getElementById("success");
-    successDiv.innerHTML = "";
-  });
-
-  var tabLinks = document.querySelectorAll('a[data-toggle="tab"]');
-  tabLinks.forEach(function (tabLink) {
-    tabLink.addEventListener("click", function (e) {
-      e.preventDefault();
-      var tabId = this.getAttribute("href").substring(1);
-      document.getElementById(tabId).classList.add("show", "active");
+      // Send email using EmailJS
+      emailjs.send("service_62ily9j", "template_vj7260h", formData).then(
+        function (response) {
+          document.getElementById("success").textContent =
+            "Your message has been sent successfully!";
+        },
+        function (error) {
+          console.log("FAILED...", error);
+          alert(
+            "There was an error while sending your message. Please try again later."
+          );
+        }
+      );
     });
-  });
-});
+})();
--- assets/js/main.js
+++ assets/js/main.js
@@ -48,3 +48,41 @@ window.onload = function () {
   setTimeout(hide_alert, 3500);
 };
 // End Alert
+
+// Start Text Animation
+document.addEventListener("DOMContentLoaded", function () {
+  // Wait for the DOM content to be fully loaded
+  var typedTextOutput = document.getElementById("typed-text-output");
+  var typedText = document.getElementById("typed-text");
+
+  if (typedTextOutput && typedText) {
+    // If the elements are found
+    var words = typedText.textContent.split(", "); // Split text into individual words
+    var currentWordIndex = 0;
+    var currentLetterIndex = 0;
+
+    function displayNextLetter() {
+      var currentWord = words[currentWordIndex];
+      if (currentLetterIndex < currentWord.length) {
+        typedTextOutput.textContent += currentWord[currentLetterIndex];
+        currentLetterIndex++;
+        setTimeout(displayNextLetter, 100); // Set timeout to display the next letter after 100 milliseconds
+      } else {
+        // If all letters of the current word are displayed, move to the next word
+        typedTextOutput.style.transition = "opacity 0.3s"; // Apply transition effect
+        setTimeout(function () {
+          typedTextOutput.textContent = ""; // Clear the output
+          typedTextOutput.style.transition = "none"; // Remove transition effect temporarily to reset position
+          currentWordIndex = (currentWordIndex + 1) % words.length; // Move to the next word (looping back to the beginning if necessary)
+          currentLetterIndex = 0; // Reset the letter index for the new word
+          displayNextLetter(); // Display the next word
+        }, 1500); // Set timeout to start displaying the next word after 0.3 second transition
+      }
+    }
+
+    // Start displaying letters
+    displayNextLetter();
+  }
+});
+
+// End Text Animation
